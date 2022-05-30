dc45a743882089b8625a6824f60860060a48e3f0
SEANYB4 <bainsean@outlook.com>
2022-05-30 10:46:11 +0100

    added shop animation

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ const background = new Sprite({
 })
 
 
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 128
+    },
+    imageSrc: './shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
+
 const player = new Fighter({
     position: {
     x: 0,
@@ -119,6 +129,7 @@ function animate() {
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height)
     background.update();
+    shop.update();
     player.update()
     enemy.update()
     
